3369ac120f0222341b1928395c77b835f8a0afc4
zhaozongyun <fuyunzzy@163.com>
2024-07-19 11:53:43 +0800

    TCP和UDP，服务器和客户端，全都有

---
+++ NetModuleTCP.cs
@@ -0,0 +1,143 @@
+﻿using HT.Framework;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using Debug = UnityEngine.Debug;
+using System;
+using System.Threading;
+
+/// <summary>
+/// 网络模块
+/// </summary>
+[CustomModule("NetModuleTCP", true)]
+public partial class NetModuleTCP : CustomModuleBase
+{
+    private static NetModuleTCP _instance;
+    public static NetModuleTCP Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = Main.m_CustomModule["NetModuleTCP"].Cast<NetModuleTCP>();
+            }
+
+            return _instance;
+        }
+    }
+
+    private TcpClient tcpClient;
+    private IPEndPoint endPoint;
+    private NetworkStream stream;
+    private byte[] receiveBuffer;
+    private Thread thread;
+
+    public override void OnInit()
+    {
+        base.OnInit();
+    }
+
+    public override void OnReady()
+    {
+        base.OnReady();
+
+        tcpClient = new TcpClient();
+        receiveBuffer = new byte[1024];
+        Debug.Log("发起连接");
+
+        tcpClient.BeginConnect(IPAddress.Parse("192.168.0.33"), 11000, ConnectCallback, tcpClient);
+    }
+
+    private void ConnectCallback(IAsyncResult ar)
+    {
+        try
+        {
+            tcpClient.EndConnect(ar);
+            Debug.Log("连接服务器成功");
+            stream = tcpClient.GetStream();
+
+
+            // 接收消息，方式一，异步调用 + 尾递归
+            Receive();
+
+            // 接收消息，方式二，线程阻塞
+            //thread = new Thread(new ThreadStart(ReceiveThread));
+            //thread.Start();
+        }
+        catch (Exception ex)
+        {
+            Debug.Log("连接服务器异常：" + ex.Message);
+        }
+    }
+
+    void Receive()
+    {
+        stream.BeginRead(receiveBuffer, 0, receiveBuffer.Length, ReadCallback, stream);
+    }
+
+    private void ReadCallback(IAsyncResult ar)
+    {
+        int length = stream.EndRead(ar);
+        string message = System.Text.Encoding.UTF8.GetString(receiveBuffer, 0, length);
+        Debug.Log($"从服务器接收到消息：" + message);
+
+        // 尾递归
+        Receive();
+    }
+
+    //接收线程
+    void ReceiveThread()
+    {
+        while (true)
+        {
+            int length = tcpClient.Available;
+            if (tcpClient.Connected && length > 0)
+            {
+                stream.Read(receiveBuffer, 0, length);
+                string message = System.Text.Encoding.UTF8.GetString(receiveBuffer, 0, length);
+                Debug.Log($"从服务器接收到消息：" + message);
+            }
+        }
+    }
+
+    // 发送
+    public void Send(string message)
+    {
+        byte[] sendBytes = System.Text.Encoding.UTF8.GetBytes(message);
+
+        // 发送方式一，用 NetworkStream
+        stream.Write(sendBytes, 0, sendBytes.Length);
+
+        // 发送方式二，用 TcpClient 的 Client（Socket）
+        //int length = tcpClient.Client.Send(sendBytes);
+        //Console.WriteLine($"发送字节数：{length}");
+    }
+
+    public void Disconnect()
+    {
+        if (tcpClient != null)
+            tcpClient.Close();
+    }
+
+    public override void OnTerminate()
+    {
+        base.OnTerminate();
+        Log.Info("退出 NetModuleTCP");
+
+        if (thread != null)
+        {
+            thread.Abort();
+            thread = null;
+        }
+        if (stream != null)
+        {
+            stream.Close();
+            stream = null;
+        }
+        if (tcpClient != null)
+        {
+            tcpClient.Close();
+            tcpClient = null;
+        }
+    }
+}
---
+++ NetModuleUDP.cs
@@ -0,0 +1,82 @@
+﻿using HT.Framework;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Threading;
+
+/// <summary>
+/// 网络模块
+/// </summary>
+[CustomModule("NetModuleUDP", true)]
+public partial class NetModuleUDP : CustomModuleBase
+{
+    private static NetModuleUDP _instance;
+    public static NetModuleUDP Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = Main.m_CustomModule["NetModuleUDP"].Cast<NetModuleUDP>();
+            }
+
+            return _instance;
+        }
+    }
+
+    //服务器端口
+    private const int port = 11000; //服务器和客户端在一台电脑，客户端绑定的端口不能与服务器相同，否则端口被占用报错
+    private UdpClient udpClient;
+    private IPEndPoint endPoint;
+    private Thread thread;
+
+    public override void OnInit()
+    {
+        base.OnInit();
+    }
+
+    public override void OnReady()
+    {
+        base.OnReady();
+
+        udpClient = new UdpClient(22000);   //绑定一个端口
+        endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), port);  //接收远程ip和端口的数据
+
+        //开启一个线程接收服务器消息，否则主线程卡死
+        thread = new Thread(new ThreadStart(Receive));
+        thread.Start();
+    }
+
+    //接收
+    void Receive()
+    {
+        while (true)
+        {
+            byte[] bytes = udpClient.Receive(ref endPoint);
+
+            // 将接收到的消息解析为字符串，为坐标数据，例如 10,15;20;30
+            string message = System.Text.Encoding.UTF8.GetString(bytes);
+            Log.Info("从服务器接收到消息：" + message);
+        }
+    }
+
+    /// <summary>
+    /// 发送网络消息
+    /// </summary>
+    public void Send(string message)
+    {
+        byte[] bytes = System.Text.Encoding.UTF8.GetBytes(message);
+        udpClient.Send(bytes, bytes.Length, endPoint);
+    }
+
+    public override void OnTerminate()
+    {
+        base.OnTerminate();
+
+        Log.Info("退出 NetModuleUDP");
+        if (thread != null)
+            thread.Abort();
+        if (udpClient != null)
+            udpClient.Close();
+    }
+}
