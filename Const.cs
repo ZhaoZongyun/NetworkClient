fc1677d5bb4dab27e95d2367a60db86de8c7d42f
ZhaoZongyun <fromjava@163.com>
2024-12-23 10:38:30 +0800

    Create Const.cs

---
+++ Const.cs
@@ -0,0 +1,11 @@
+/// <summary>
+/// 存放一些常量
+/// </summary>
+public static class Const
+{
+    public static string serverIp = "127.0.0.1";
+    public static int udp_serverPort = 12345;
+    public static int udp_clientPort = 22345;
+
+    public static int tcp_serverPort;
+}
