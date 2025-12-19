a3668d9448c03e81c109bcf0cf9b8926b93d1b54
ZhaoZongyun <fromjava@163.com>
2025-12-19 17:30:01 +0800

    Update UDP server and client ports

--- Const.cs
+++ Const.cs
@@ -4,8 +4,8 @@
 public static class Const
 {
     public static string serverIp = "127.0.0.1";
-    public static int udp_serverPort = 12345;
-    public static int udp_clientPort = 22345;
+    public static int udp_serverPort = 7777;
+    public static int udp_clientPort = 0;
 
     public static int tcp_serverPort;
 }
